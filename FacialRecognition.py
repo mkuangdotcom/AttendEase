5cd7b354f5fe0cfd0bfb1c02e384165b8ed5a8da
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-01-26 18:09:14 +0800

    Update FacialRecognition.py

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -1,37 +1,37 @@
-
-# python library to import and used here
 import cv2
 import os
-capture = cv2.VideoCapture(0)
 
-# width capturing
-capture.set(3, 1280)
-# height capturing
-capture.set(4, 720)
-# state variable
-backgroundImg = cv2.imread('Resources/Background.png')
+# Captures video (0 for )
+capture  = cv2.VideoCapture(0) 
+
+capture.set(3, 1280) #Window size (3 for width)
+capture.set(4, 720)  #Window size (4 for height)
+backgroundImg = cv2.imread(r'Resources/Background.png')
 
 folderPathMode = 'Resources/Modes'
 listPathMode = os.listdir(folderPathMode)
-imgListMode = []
+imageListMode = []
+
 
 for path in listPathMode:
-    imgListMode.append(cv2.imread(os.path.join(folderPathMode, path)))
-    
-print(listPathMode)
+    imageListMode.append(cv2.imread(os.path.join(folderPathMode, path)))
 
-# conditional capture  
+print(imageListMode)
+
 while True:
     success, image = capture.read()
 
-    # webcam over the background
-    image = cv2.resize(image,(640,480))
-    backgroundImg[162: 162+480, 55: 55+640] = image
-    backgroundImg[44: 44+633, 808: 808+414 ] = imgListMode[0]
+    image = cv2.resize(image, (640, 480))
 
-    # showing a window for webcam 
-    # cv2.imshow("My Webcam", image)
+    #onto the position of 162:162 + 480, 55:55 + 640, you are pasting your web cam over the background image
+    backgroundImg[162:162 + 480, 55:55 + 640] = image
 
-    # showing window for background image
+    backgroundImg[44:44+633, 808:808 +414] = imageListMode[0]
+
+
+    # show a window for your webcam
+    # cv2.imshow("My WebCam", image)
+
+    # show a window for your system
     cv2.imshow("Attendance System", backgroundImg)
     cv2.waitKey(1)
