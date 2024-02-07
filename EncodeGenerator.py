7a95b225afce9eff748471cf8c84df833a24c5e8
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-02-07 11:33:48 +0800

    Update EncodeGenerator.py

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,76 +1,59 @@
+import cv2
+import face_recognition
+import pickle
+import os
 import firebase_admin
-from firebase_admin import credentials
 from firebase_admin import db
+from firebase_admin import storage
+from firebase_admin import credentials
 
 cred = credentials.Certificate("serviceAccountKey.json",)
+
 firebase_admin.initialize_app(cred, {
-    'databaseURL' : "https://attendanceproject-b8993-default-rtdb.firebaseio.com/",
+    'databaseURL': "https://attendanceproject-b8993-default-rtdb.firebaseio.com/",
     'storageBucket': "attendanceproject-b8993.appspot.com"
 })
 
-ref = db.reference("Students")
-# Python dictionary
-data = {
-    "TP011111":
-    {
-        "name" : "Lee Wen Han",
-        "major" : "CS(AI)",
-        "starting_year" : 2021,
-        "total_attendance" : 20,
-        "grades" : "A",
-        "year" : 2,
-        "last_attendance_taken" : "2024-01-26 16:10:30",
-    },
-    "TP012345":
-    {
-        "name" : "Foo Ming Kuang",
-        "major" : "CS(SE)",
-        "starting_year" : 2023,
-        "total_attendance" : 10,
-        "grades" : "A+",
-        "year" : 2,
-        "last_attendance_taken" : "2024-01-26 16:10:30",
-    },
-    "TP063338":{
-        "name": "Dalton Gan",
-        "major": "SE",
-        "starting_year": 2024,
-        "total_attendance": 4,
-        "standing": "A++",
-        "year": 2,
-        "last_attendance_time": "2024-01-02 12:06:02"
-    },
-    "TP068713":{
-        "name": "Suzanne Lai",
-        "major": "AI",
-        "starting_year": 2022,
-        "total_attendance": 10,
-        "standing": "A",
-        "year": 2,
-        "last_attendance_time": "2024-01-02 15:09:02"
-    },
-    "TP088888":{
-        "name": "Karina",
-        "major": "Slay",
-        "starting_year": 2024,
-        "total_attendance": 10,
-        "standing": "A+",
-        "year": 1,
-        "last_attendance_time": "2024-01-09 06:06:06"
-    },
-    "TP054321":{
-        "name": "Elon Musk",
-        "major": "Ruining Lives",
-        "starting_year": 1995,
-        "total_attendance": 10,
-        "standing": "A+",
-        "year": 29,
-        "last_attendance_time": "2024-01-08 13:10:30"
-    },
-}
+folderPathImages = 'Images'
+listPathImages = os.listdir(folderPathImages)
+imgListImages = []
 
-# This is how to "unzip a dictionary in Python"
-# writing the dictionary details into the realtime database
-for key, value in data.items():
-    # Storing the vaue in the dictionary "children" for the key in the ref you created ("Students")
-    ref.child(key).set(value)
+#Extracting Student ID from each picture
+studentIDs = []
+
+for path in listPathImages:
+    imgListImages.append(cv2.imread(os.path.join(folderPathImages,path)))
+    #taking only the first element after splitting text and storing 
+    #that into student IDs
+    studentIDs.append(os.path.splitext(path)[0])
+
+    # you're already going through the images one by one here, so you might as well use this loop to store them as well
+    # join together the full filename using folderPathImages as well as each individual path
+    fileName = f'{folderPathImages}/{path}'
+    # the following code will retrieve a reference to your established cloud storage
+    bucket = storage.bucket()
+    # a reference to a specific object is created
+    blob = bucket.blob(fileName)
+    # the file with the fileName is uploaded to reference "blob" created in your cloud storage
+    blob.upload_from_filename(fileName)
+
+
+def generateEncodings(images):
+    encodingsList = []
+
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)       # converting color format RGB (default BGR)
+        encode = face_recognition.face_encodings(img)[0]    
+        encodingsList.append(encode)
+
+    return encodingsList
+
+encodingsListKnown = generateEncodings(imgListImages)
+encodingsListWithIDs = [encodingsListKnown, studentIDs]
+
+# opening a new file called EncodingsFile.p in WRITE MODE
+encodingFile = open("EncodingsFile.p","wb")
+# using pickle to dump into the file your encodings list
+pickle.dump(encodingsListWithIDs, encodingFile)
+# closing the file 
+encodingFile.close()
