c2a761ec460dc205d21197c16c0f5004ae6c9a48
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-03-12 13:06:55 +0800

    Create badge.js

---
+++ badge.js
@@ -0,0 +1,28 @@
+{
+  "type": "Assertion",
+  "id": "https://api.badgr.io/public/assertions/J3TqFW32QLSGkPPLX5OySw",
+  "@context": "https://w3id.org/openbadges/v2",
+  "recipient": {
+    "type": "email",
+    "salt": "wBXFCSzDIPuJfMw3",
+    "hashed": true,
+    "identity": "sha256$8fb3e1fed047968a40c07d1fe47e5a5128ff71c2b17399e4583b0ee771f0cc19"
+  },
+  "badge": "https://api.badgr.io/public/badges/W6ybvbWoTHGI64xTfFp7Vg",
+  "issuedOn": "2024-03-12T01:38:38.342Z",
+  "image": {
+    "id": "https://api.badgr.io/public/assertions/J3TqFW32QLSGkPPLX5OySw/image"
+  },
+  "revoked": false,
+  "verification": {
+    "type": "HostedBadge"
+  },
+  "extensions:recipientProfile": {
+    "@context": "https://openbadgespec.org/extensions/recipientProfile/context.json",
+    "type": [
+      "Extension",
+      "extensions:RecipientProfile"
+    ],
+    "name": "FOO MING KUANG"
+  }
+}
